3e820a42f8968406921316af9605678eff5e84d7
Mothball7205 <158273459+Mothball7205@users.noreply.github.com>
2026-08-02 01:36:49 +0200

    chore: bump version to 4.1.0

--- dist/build.user.js
+++ dist/build.user.js
@@ -4,7 +4,7 @@
 // @namespace https://github.com/SkyCloudDev
 // @author SkyCloudDev
 // @description Downloads images and videos from posts
-// @version 4.0.0
+// @version 4.1.0
 // @updateURL https://github.com/SkyCloudDev/ForumPostDownloader/raw/main/dist/build.user.js
 // @downloadURL https://github.com/SkyCloudDev/ForumPostDownloader/raw/main/dist/build.user.js
 // @icon https://simp4.cuckcapital.cr/simpcityIcon192.png
